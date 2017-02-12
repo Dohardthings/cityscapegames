916c218042d11cbc3736d10fb0178843911a70e4
Daniel McBryde <mcbryded@gmail.com>
2017-02-12 14:29:42 -0600

    added routes for chapter 1 and linked them together

--- app/router.js
+++ app/router.js
@@ -9,7 +9,24 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('index', { path: '/' });
 
-  this.route('TermsConditions');
+
+  this.route('cities');
+  this.route('nashville-adventures');
+  this.route('knights-of-infernal-order', function() {
+    this.route('paywall');
+    this.route('TermsConditions');
+    this.route('rules');
+    this.route('adventure', function() {
+      this.route('prologue');
+      this.route('chapter1-1');
+      this.route('chapter1-2');
+      this.route('chapter1-3');
+      this.route('chapter1-4');
+      this.route('chapter1-5');
+      this.route('chapter1-6');
+      this.route('chapter1-7');
+    });
+  });
 });
 
 export default Router;
---
+++ tests/unit/knights-of-infernal-order/adventure/prologue/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:knights-of-infernal-order/adventure/prologue', 'Unit | Route | knights of infernal order/adventure/prologue', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
