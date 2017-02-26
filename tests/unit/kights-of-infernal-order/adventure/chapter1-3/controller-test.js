d6b222f917bf80cd5b0940cf9618eb3f22716815
Daniel McBryde <mcbryded@gmail.com>
2017-02-26 16:32:58 -0600

    not sure what's happening after I switched back to branch dan after experimenting on two other brances I'm commiting to see if it fixes the editor. all the changes from the other branches are still visible

---
+++ tests/unit/kights-of-infernal-order/adventure/chapter1-3/controller-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:kights-of-infernal-order/adventure/chapter1-3', 'Unit | Controller | kights of infernal order/adventure/chapter1 3', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
