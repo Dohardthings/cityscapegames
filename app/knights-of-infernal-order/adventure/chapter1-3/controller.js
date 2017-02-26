36aca544059a0fdfae4f41a0d1f3f2d779457539
Daniel McBryde <mcbryded@gmail.com>
2017-02-26 17:11:05 -0600

    add hint buttons with modals to chapter 1-3

--- app/knights-of-infernal-order/adventure/chapter1-3/controller.js
+++ app/knights-of-infernal-order/adventure/chapter1-3/controller.js
@@ -2,9 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isShowingModal: false,
+  isShowingModal2: false,
+  isShowingModal3: false,
   actions: {
     toggleModal: function() {
       this.toggleProperty('isShowingModal');
-    }
+    },
+    toggleModal2: function() {
+      this.toggleProperty('isShowingModal2');
+    },
+    toggleModal3: function() {
+      this.toggleProperty('isShowingModal3');
+    },
   }
 });
