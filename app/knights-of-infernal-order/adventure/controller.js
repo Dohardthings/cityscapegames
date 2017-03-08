b0f4907586dd7667c3af4af6625ffefde787041b
Daniel McBryde <mcbryded@gmail.com>
2017-03-08 17:41:59 -0600

    fix timer and add production build.  Production build does not load.  Not usre why, it could be a surge thing.  Will keep troubleshooting.

--- app/knights-of-infernal-order/adventure/controller.js
+++ app/knights-of-infernal-order/adventure/controller.js
@@ -1,92 +1,64 @@
 import Ember from 'ember';
 export default Ember.Controller.extend({
 
-  init: function () {
-      this._super();
-      Ember.run.schedule("afterRender",this,function() {
-        this.send("foo");
-      });
-    },
+  init: function() {
+    this._super();
+    Ember.run.schedule("afterRender", this, function() {
+      this.send("foo");
+    });
+  },
 
-actions:{
+  actions: {
+    foo() {
 
-   foo: function getMeATimer(){
-
-  function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 5);
-    return {
-      'total': t,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
+      function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 5);
+        return {'total': t, 'hours': hours, 'minutes': minutes, 'seconds': seconds};
       }
-    }
+      let deadline = localStorage.getItem('deadline');
+      if (!deadline) {
+        deadline = (new Date(Date.parse(new Date()) + 14400000)).toString();
+        localStorage.setItem('deadline', deadline);
+      }
 
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-  }
+      function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
 
-    if (localStorage.getItem('deadline')){
-      var deadline = localStorage.getItem('deadline');
+
+
+        function updateClock() {
+          var t = getTimeRemaining(endtime);
+
+          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+          if (t.total <= 0) {
+            clearInterval(timeinterval);
+          }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+      }
+
       initializeClock('clockdiv', deadline);
-      var text = document.getElementById('clockdiv');
-      text.addEventListener("load", init(), false);
-
-      function changeColor() {
-          text.style.background = "#F00";
-      }
-
-      function init() {
-        setTimeout(changeColor, 14400000);
-      }
-
+      // var text = document.getElementById('clockdiv');
+      // text.addEventListener("load", init(), false);
+      //
+      // function changeColor() {
+      //   text.style.background = "#F00";
+      // }
+      //
+      // function init() {
+      //   setTimeout(changeColor, 14400000);
+      // }
     }
-
-   if (!localStorage.getItem('deadline')) {
-     var deadline = new Date(Date.parse(new Date()) + 14400000);
-     deadline = localStorage.setItem('deadline', deadline);
-
-
-
-  initializeClock('clockdiv', deadline);
-
-  var text = document.getElementById('clockdiv');
-  text.addEventListener("load", init(), false);
-
-  function changeColor() {
-      text.style.background = "#F00";
   }
-  function init() {
-    setTimeout(changeColor, 14400000);
-  }
-  function reload() {
-    window.location.reload(true);
-  }
-  reload();
-}
-},
-
-
-}
 });
